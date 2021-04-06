9a54d57ade4b5d8abb0dca6b54390c8e25b5e386
roliboy <roliboy@protonmail.com>
2021-04-06 20:58:57 +0300

    abstraction layer for api interaction and register page

--- api.js
+++ api.js
@@ -1,32 +1,55 @@
-const api = axios.create({
+// TODO: handle refresh tokens
+
+client = axios.create({
     baseURL: 'https://kind-wind-83282.pktriot.net/'
 })
 
-// TODO: handle refresh tokens
-api.interceptors.request.use(
+endpoints = {
+    login: 'auth/login/',
+    logout: 'auth/logout/',
+    register: 'auth/register/',
+    getConferences: 'restricted/conferences/',
+}
+
+api = {
+    login: (username, password) => client.post(endpoints.login, {username, password}),
+    logout: () => client.post(endpoints.logout),
+    register: (username, email, password1, password2) => client.post(endpoints.register, {username, email, password1, password2}),
+    getConferences: () => client.get(endpoints.getConferences),
+}
+
+client.interceptors.request.use(
     config => {
-        access_token = window.localStorage.getItem("x-jwt-access-tokens")
-        if (access_token)
+        access_token = window.localStorage.getItem('x-jwt-access-token')
+        // TODO: refactor this
+        if (access_token && config.url != endpoints.login && config.url != endpoints.register)
             config.headers['Authorization'] = `Bearer ${access_token}`
+
         return config
     },
     error => Promise.reject(error)
 )
 
-// TODO: handle refresh tokens
-// TODO: intercept access token response
-api.interceptors.response.use(
-    response => response,
+client.interceptors.response.use(
+    response => {
+        // TODO: check status codes and refactor
+        if (response.config.url == endpoints.login || response.config.url == endpoints.register)
+            window.localStorage.setItem('x-jwt-access-token', response.data.access_token)
+
+        if (response.config.url == endpoints.logout)
+            window.localStorage.removeItem('x-jwt-access-token')
+
+        return response
+    },
     error => {
-        if (error.response.status == 401)
-            window.location.href = 'login.html'
+        console.warn('ERROR INTERCEPTED')
+        console.log(`${error.response.config.url} ${error.response.status} ${error.response.statusText}`)
+        console.log(error.response)
+        console.log(error.request)
+        console.log(error.config)
+        // TODO: redirect to login when accessing unauthorized endpoint?
+        // if (error.response.status == 401)
+            // window.location.href = 'login.html'
         return Promise.reject(error)
     }
 )
-
-// TODO: something like this?
-// api.endpoints = {
-//     getConferences: () => {
-//         return api.get()...
-//     }
-// }
